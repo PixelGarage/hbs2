b74d6f58a985f7ecdb52da0f999109dfb2f7232a
Ralph Moser <ralph@ramosoft.ch>
2017-04-25 19:03:42 +0200

    - info events implemented

--- all/themes/pixelgarage/templates/views-bootstrap-tab-plugin-style--course-tabs.tpl.php
+++ all/themes/pixelgarage/templates/views-bootstrap-tab-plugin-style--course-tabs.tpl.php
@@ -1,7 +1,10 @@
-<div id="views-bootstrap-tab-<?php print $id ?>" class="<?php print $classes ?>">
+<div id="views-bootstrap-tab-course-tabs" class="<?php print $classes ?>">
   <ul class="nav nav-<?php print $tab_type?> <?php if ($justified) print 'nav-justified' ?>">
-    <li class="active">
+    <li class="tab-startdaten active">
       <a href="#tab-startdaten" data-toggle="tab">Startdaten</a>
+    </li>
+    <li class="tab-info-events">
+      <a href="#tab-info-events" data-toggle="tab">Informationsanlässe</a>
     </li>
     <?php foreach ($tabs as $key => $tab): ?>
      <li>
@@ -12,6 +15,9 @@
   <div class="tab-content">
     <div class="tab-pane active" id="tab-startdaten">
       <?php print views_embed_view('course_times'); ?>
+    </div>
+    <div class="tab-pane" id="tab-info-events">
+      <?php print views_embed_view('course_times', 'block_info_events'); ?>
     </div>
     <?php foreach ($rows as $key => $row): ?>
       <div class="tab-pane" id="tab-<?php print "{$id}-{$key}" ?>">
--- all/themes/pixelgarage/templates/views-view--course-tabs.tpl.php
+++ all/themes/pixelgarage/templates/views-view--course-tabs.tpl.php
@@ -34,14 +34,20 @@
 			<?php print $rows; ?>
 		<?php else: ?>
 			<ul class="nav nav-tabs">
-				<li class="active">
+				<li class="tab-startdaten active">
 					<a href="#tab-startdaten" data-toggle="tab">Startdaten</a>
 				</li>
+        <li class="tab-info-events">
+          <a href="#tab-info-events" data-toggle="tab">Informationsanlässe</a>
+        </li>
 			</ul>
 			<div class="tab-content">
 				<div class="tab-pane active" id="tab-startdaten">
 					<?php print views_embed_view('course_times'); ?>
 				</div>
+        <div class="tab-pane" id="tab-info-events">
+          <?php print views_embed_view('course_times', 'block_info_events'); ?>
+        </div>
 			</div>
 		<?php endif; ?>
 	</div>
